032e084c41efae07bfdd35029013b330b5fd3bf6
wuyu <191287278@qq.com>
2017-05-06 11:07:35 +0800

    ratelimit

--- src/main/java/com/alibaba/boot/dubbo/DubboAutoConfiguration.java
+++ src/main/java/com/alibaba/boot/dubbo/DubboAutoConfiguration.java
@@ -2,20 +2,13 @@ package com.alibaba.boot.dubbo;
 
 import com.alibaba.boot.dubbo.endpoint.DubboEndpoint;
 import com.alibaba.boot.dubbo.health.DubboHealthIndicator;
-import com.alibaba.boot.dubbo.zipkin.ConsumerSpan;
-import com.alibaba.boot.dubbo.zipkin.DubboSpanExtractor;
-import com.alibaba.boot.dubbo.zipkin.DubboSpanInjector;
-import com.alibaba.boot.dubbo.zipkin.ProviderSpan;
 import com.alibaba.dubbo.config.ApplicationConfig;
 import com.alibaba.dubbo.config.ProtocolConfig;
 import com.alibaba.dubbo.config.RegistryConfig;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.context.annotation.Bean;
 
 @EnableConfigurationProperties(DubboProperties.class)
@@ -53,28 +46,4 @@ public class DubboAutoConfiguration {
         return new DubboHealthIndicator();
     }
 
-
-//    @Bean
-//    @ConditionalOnClass(Tracer.class)
-//    public DubboSpanInjector dubboSpanInjector() {
-//        return new DubboSpanInjector();
-//    }
-//
-//    @Bean
-//    @ConditionalOnClass(Tracer.class)
-//    public DubboSpanExtractor dubboSpanExtractor() {
-//        return new DubboSpanExtractor();
-//    }
-//
-//    @Bean
-//    @ConditionalOnBean(value = {DubboSpanInjector.class,DubboSpanExtractor.class})
-//    public ProviderSpan providerSpan() {
-//        return new ProviderSpan();
-//    }
-//
-//    @Bean
-//    @ConditionalOnBean(value = {DubboSpanInjector.class,DubboSpanExtractor.class})
-//    public ConsumerSpan consumerSpan() {
-//        return new ConsumerSpan();
-//    }
 }
